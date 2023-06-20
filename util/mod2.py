c2764d5c5b668c9703a5a34edfdc720e512a4d92
Ikemen Mas Kot <maskot1977@gmail.com>
2023-06-20 13:59:58 +0900

    Create mod2.py

---
+++ util/mod2.py
@@ -0,0 +1,18 @@
+import math
+
+print("Hello world!")
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, p):
+        return math.sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
+
+    def midpoint(self, p):
+        return Point((self.x + p.x) / 2, (self.y + p.y) /2)
+
+
+def my_name(z, w):
+    print("I'm", z, w)
